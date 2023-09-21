0e7e1b598c939dba13028cc0a416c5e70ab45ec9
Matas Kirvaitis <kirvaitism@gmail.com>
2023-09-21 20:47:10 +0300

    Added check if DB exists and creation

--- index.ts
+++ index.ts
@@ -2,19 +2,40 @@ import startClient from './client/client';
 import startServer from './server/server';
 import 'dotenv/config';
 import { AppDataSource } from './datasource';
-import LogService from './src/services/LogService';
+import { DataSource } from 'typeorm';
 
 //incorrect logger method used in most places, need to recheck
 
 (async () => {
-    //Add database creation if does not exist in application start.
     //Add Docker Compose.
     try {
+        const tempDataSource = new DataSource ({
+            type: 'postgres',
+            host: process.env.PG_HOST,
+            port: Number(process.env.PG_PORT),
+            username: process.env.PG_USER,
+            password: process.env.PG_PASSWORD,
+            database: 'postgres',
+        });
+    
+        await tempDataSource.initialize();
+    
+        const dbExists = await tempDataSource.query(
+            `SELECT datname FROM pg_database WHERE datname='${process.env.PG_DATABASE}'`
+        );
+
+        if (dbExists.length === 0) {
+            await tempDataSource.query(`CREATE DATABASE ${process.env.PG_DATABASE}`);
+            console.log(`Database ${process.env.PG_DATABASE} created successfully`);
+        }
+
+        await tempDataSource.destroy();
+
         await AppDataSource.initialize();
         console.log('Data Source Initialized');
 
-        // startServer();
-        // startClient();
+        startServer();
+        startClient();
     } catch(err) {
         if (err instanceof Error) {
             console.log(err.message);

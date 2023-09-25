a2f5eddb92cc8f685f1cfff13d2d991685e1a12f
Matas Kirvaitis <kirvaitism@gmail.com>
2023-09-25 14:22:07 +0300

    Updated Server logic

--- server/server.ts
+++ server/server.ts
@@ -3,7 +3,7 @@ import Logger from '../logger';
 import csvToJSON from '../csvToJson';
 import fs from 'fs';
 import 'dotenv/config';
-import url from 'url';
+import FileService from '../src/services/FileService';
 
 const port = 1337;
 
@@ -12,10 +12,10 @@ const port = 1337;
 //File passed to csvToJson with output file name same as input file name - done
 //Conversion result file changed into URL with format 'http://localhost:1337/downloads?file=<JSON converted file name> - done
 //This URL is returned in the response WITHOUT awaiting for the conversion to be done - done
-//This URL is also saved to DB along with 'Initial Conversion Timestamp' when entry is first created
-//multiple entries with same URL means the file has been re-converted after the initial file and thus is overwriten.
-//If URL is accessed before the conversion is done, message is displaying prompting the user to wait
-//If page is refreshed or opened after conversion is done, it automatically downloads the converted file
+//This URL is also saved to DB along with 'Conversion Timestamp' when entry is first created - done
+//multiple entries with same URL means the file has been re-converted after the initial file and thus is overwriten. - done
+//If URL is accessed before the conversion is done, message is displaying prompting the user to wait - done
+//If page is refreshed or opened after conversion is done, it automatically downloads the converted file - in progress
 //User can access the same page in the future, the server checks with DB if file with this name exists and returns the file
 //Additionally server returns a message 'File <JSON converted file name> is ready to download, file converted at <timestamp>'
 //If file was overwriten since the initial conversion, message is returned 'File <JSON converted file name> is ready to download, the file has been overwritten since first conversion, latest conversion at <timestamp>'
@@ -25,7 +25,9 @@ export default async function startServer() {
         process.env.LOG_TO_CONSOLE === 'false' ? false : true,
         process.env.SAVE_TO_DB === 'true' ? true : false);
 
-    const server = http.createServer((request, response) => {
+    const server = http.createServer(async (request, response) => {
+        const fileService = new FileService();
+
         if (request.url?.split('?')[0] === '/upload') {
             // Need to send argument ?headers=true if headers are in file otherwise automatically set to false
             if (request.method === 'POST' && request.headers['content-type'] === 'text/csv') {
@@ -52,31 +54,49 @@ export default async function startServer() {
                 })
                 .on('end', () => {
                     writeStream.close();
-                    response.writeHead(200, { 'Content-Type': 'text/plain'});
-                    response.end('File saved successfully');
-                    logger.info('File saved successfully');
+                    let downloadURL = `http://localhost:${port}/download?file=${fileName}`;
+
+                    if(params.get('headers') === 'true') {
+                        csvToJSON(fileName, fileName, true)
+                        .then(() => {
+                            return fileService.create({url: downloadURL});
+                        });
+                    } else {
+                        csvToJSON(fileName, fileName, false)
+                        .then(() => {
+                            return fileService.create({url: downloadURL});
+                        });
+                    }
+
+                    response.writeHead(200, { 'Content-Type': 'text/plain' });
+                    response.end(`File is being converted and once done will be available at: ${downloadURL}`);
                 })
                 .on('error', err => {
                     logger.error(err.message);
                 });
-
-                console.log(params.get('headers'));
-
-                if(params.get('headers') === 'true') {
-                    csvToJSON(fileName, fileName, true);
-                } else {
-                    csvToJSON(fileName, fileName, false);
-                }
-
-                // Needs to push this URL to DB
-                let downloadURL = `http://${process.env.PG_HOST}:${process.env.PG_PORT}/downloads?file=${fileName}`;
-
-                response.writeHead(200, { 'Content-Type': 'text/plain' });
-                response.end(`File is being converted and once done will be available at: ${downloadURL}`);
+            } else {
+                response.writeHead(400, {'Content-Type': 'text/plain'});
+                response.end('No valid CSV file found, please attach valid file and configure request headers');
             }
         } else if (request.url?.split('?')[0] === '/download') {
-            if (request.method === 'GET' && request.url?.includes('/downloads')) {
-                // logic for checking DB and downloading
+            if (request.method === 'GET' && request.url.split('?').length !== 0) {
+                let params = new URLSearchParams(request.url.split('?')[1]);
+                let fileName = params.get('file') ?? '';
+                let foundFiles = await fileService.findAll({url: fileName});
+
+                if (fileName == '' || foundFiles.length === 0) {
+                    response.writeHead(404, {'Content-Type': 'text/plain'});
+                    response.end('File not found');
+                } else if (foundFiles.length === 1){
+                    //Return JSON file in response
+                } else if (foundFiles.length > 1) {
+                    //Return JSON file in response and notify that file is overwritten since initial conversion
+                    response.writeHead(200, {'Content-Type': 'text/plain'});
+                    response.end('Found more than 1 file with this name in DB');
+                }
+            } else {
+                response.writeHead(400, {'Content-Type': 'text/plain'});
+                response.end('Invalid HTTP method used or valid arguments not passed');
             }
         } else {
             response.writeHead(404, { 'Content-Type': 'text/plain'});

e01b9c9cf7aba7dfdaede6cc0dc0b8dd944eab63
Matas Kirvaitis <kirvaitism@gmail.com>
2023-09-27 13:27:16 +0300

    Added clusters to run separate instances of server

--- index.ts
+++ index.ts
@@ -2,35 +2,51 @@ import startServer from './src/server/server';
 import 'dotenv/config';
 import { AppDataSource } from './src/datasource';
 import { DataSource } from 'typeorm';
+import cluster from 'cluster';
+import os from 'os';
 
 (async () => {
     try {
-        const tempDataSource = new DataSource({
-            type: 'postgres',
-            host: process.env.PG_HOST,
-            port: Number(process.env.PG_PORT),
-            username: process.env.PG_USER,
-            password: process.env.PG_PASSWORD,
-            database: 'postgres',
-        });
+        if (cluster.isPrimary) {
+            const tempDataSource = new DataSource({
+                type: 'postgres',
+                host: process.env.PG_HOST,
+                port: Number(process.env.PG_PORT),
+                username: process.env.PG_USER,
+                password: process.env.PG_PASSWORD,
+                database: 'postgres',
+            });
+    
+            await tempDataSource.initialize();
+    
+            const dbExists = await tempDataSource.query(
+                `SELECT datname FROM pg_database WHERE datname='${process.env.PG_DATABASE}'`
+            );
+    
+            if (dbExists.length === 0) {
+                await tempDataSource.query(`CREATE DATABASE ${process.env.PG_DATABASE}`);
+                console.log(`Database ${process.env.PG_DATABASE} created successfully`);
+            }
+    
+            await tempDataSource.destroy();
 
-        await tempDataSource.initialize();
+            const numCores = os.cpus().length;
 
-        const dbExists = await tempDataSource.query(
-            `SELECT datname FROM pg_database WHERE datname='${process.env.PG_DATABASE}'`
-        );
+            for (let i=0; i < numCores; i++) {
+                cluster.fork();
+            }
 
-        if (dbExists.length === 0) {
-            await tempDataSource.query(`CREATE DATABASE ${process.env.PG_DATABASE}`);
-            console.log(`Database ${process.env.PG_DATABASE} created successfully`);
+            cluster.on('exit', (worker, code, signal) => {
+                console.log(`Worker ${worker.process.pid} died with code: ${code}, and signal: ${signal}`);
+                console.log('Starting new worker.');
+                cluster.fork();
+            });
+        } else {
+            await AppDataSource.initialize();
+            console.log('Data Source Initialized');
+
+            startServer();
         }
-
-        await tempDataSource.destroy();
-
-        await AppDataSource.initialize();
-        console.log('Data Source Initialized');
-
-        startServer();
     } catch (err) {
         if (err instanceof Error) {
             console.log(err.message);
